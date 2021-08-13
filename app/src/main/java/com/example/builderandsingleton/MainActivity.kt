3a794d679f7c0346ac5c0826a2450a80e39e37ef
zhenishkaziev <artykbaevaslan0@gmail.com>
2021-08-13 17:39:10 +0300

    Исправил недочеты

--- app/src/main/java/com/example/builderandsingleton/MainActivity.kt
+++ app/src/main/java/com/example/builderandsingleton/MainActivity.kt
@@ -6,14 +6,14 @@ import com.example.builderandsingleton.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-     lateinit var binding : ActivityMainBinding
+     private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-         binding.setText.text = PatternSingleton.getInstance()?.getData()
+         binding.setText.text = PatternSingleton.getData()
 
 
 
--- app/src/main/java/com/example/builderandsingleton/PatternSingleton.kt
+++ app/src/main/java/com/example/builderandsingleton/PatternSingleton.kt
@@ -1,22 +1,11 @@
 package com.example.builderandsingleton
 
-import android.graphics.Color
 
-class PatternSingleton private constructor() {
-
-    companion object {
-        private var instance: PatternSingleton? = null
-
-        fun getInstance(): PatternSingleton? {
-            if (instance == null)
-                instance = PatternSingleton()
-            return instance
-        }
-    }
+object PatternSingleton {
 
     fun getData(): String {
-        val builder: PatternBuilder =
-            ConfigurationBuilder().setSensitivity(200).setColor(Color.BLACK).setNumber(45).build()
-        return builder.toString()
+
+        val builder = PatternBuilder().ConfigurationBuilder().setNumber(45).setSensitivity(300).setColor(0).build()
+        return "${builder.color} ${builder.number}, ${builder.sensitivity}"
     }
 }
